3e531cbfad4277e5f74fed8803916139e800c378
Rohan Parekh <rparekh@primervoto.de>
2019-03-12 10:49:03 +0100

    added context root and thymeleaf template

--- src/main/java/com/solactive/eportal/HomeController.java
+++ src/main/java/com/solactive/eportal/HomeController.java
@@ -1,26 +1,41 @@
 package com.solactive.eportal;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
-public class HomeController {
+@RequestMapping("/eportal")
+public class HomeController
+{
 
-    @GetMapping("/list")
-    public List<String> getList() {
-        List<String> list = new ArrayList<>();
-        list.add("event 1");
-        list.add("event 2");
-        list.add("event 3");
-        return list;
+   @Value("${spring.application.name}")
+   String appName;
 
-    }
+   @GetMapping("/list")
+   public List<String> getList()
+   {
+      List<String> list = new ArrayList<>();
+      list.add("event 1");
+      list.add("event 2");
+      list.add("event 3");
+      list.add("event 4");
+      return list;
 
-    // list
+   }
+
+   @GetMapping("/home")
+   public String homePage(Model model)
+   {
+      model.addAttribute("appName", appName);
+      return "home";
+   }
+   // list
 
    // detail page
 
