7633420824a20585303407ac8ccbac5382432989
Rohan Parekh <rohan.parekh.in@gmail.com>
2019-03-12 20:31:34 +0100

    dev branch test

--- src/main/java/com/solactive/eportal/HomeController.java
+++ src/main/java/com/solactive/eportal/HomeController.java
@@ -23,6 +23,9 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 
+ */
 @RestController
 @RequestMapping("/eportal")
 public class HomeController
